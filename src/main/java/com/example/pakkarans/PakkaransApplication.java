a431d95f216c3553a7e3728dd8565539f4e269e5
Vijay Thomas <vijay_thomas@intuit.com>
2018-11-02 19:32:54 -0700

    Sample project

---
+++ src/main/java/com/example/pakkarans/PakkaransApplication.java
@@ -0,0 +1,12 @@
+package com.example.pakkarans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PakkaransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PakkaransApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/pakkarans/PakkaransApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.pakkarans;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PakkaransApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
